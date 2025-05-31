5f922c5dc83b89bafc566850b09b55d5aeec4401
anishkumarshrees <anishshrees12@gmail.com>
2025-05-31 16:38:19 +0545

    npm express and res,req

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 "# mernclass1" 
+npm(node package manager)-we can install all the package that we need like messenger, google map, esewa
+they all give the packages to make our website easy
+
+to import express in node we write
+const express = require('express');
--- app.js
+++ app.js
@@ -1,18 +1,40 @@
-const a= 10;
+const express=require("express")
+const app=express()
+
+app.get("/",function(req,res){
+    // res.send("Hello World")
+    res.json({
+        anish:"hello k xa khbr"
+    })
+})
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000")
+})
 
 
-console.log(a)
 
-//array
-const number=[10,20,30]
-//console.log(number[1]) first ko print hunxa
-//to calculate length
-console.log(number.length)
 
-//objects
-const person={
-    name : "anish",
-    age:23
-}
-//{key:value}
-console.log(person)
+
+
+
+
+
+// const a= 10;
+
+
+// console.log(a)
+
+// //array
+// const number=[10,20,30]
+// //console.log(number[1]) first ko print hunxa
+// //to calculate length
+// console.log(number.length)
+
+// //objects
+// const person={
+//     name : "anish",
+//     age:23
+// }
+// //{key:value}
+// console.log(person)
