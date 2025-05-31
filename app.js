0e68c1f5fc6729ccc535762d12b942f9befa5f2a
anishkumarshrees <anishshrees12@gmail.com>
2025-05-31 15:53:55 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const a= 10;
+
+
+console.log(a)
+
+//array
+const number=[10,20,30]
+//console.log(number[1]) first ko print hunxa
+//to calculate length
+console.log(number.length)
+
+//objects
+const person={
+    name : "anish",
+    age:23
+}
+//{key:value}
+console.log(person)
